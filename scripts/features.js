35fb2d34bed1b3e8aa9b38a79149c55d846983ea
ThiagoBarbosa05 <thiagon16@hotmail.com>
2022-04-21 15:34:11 -0300

    finish

--- scripts/features.js
+++ scripts/features.js
@@ -6,20 +6,38 @@ let featuresNavigation = document.querySelectorAll('#features-js')
 
 
 featuresNavigation[0].onclick = () => {
+
+    featuresNavigation[0].classList.add('active');
+    featuresNavigation[1].classList.remove('active');
+    featuresNavigation[2].classList.remove('active');
+
     projects.style.display = 'block';
     skills.style.display = 'none';
     contact.style.display = 'none';
+
 }
 
 featuresNavigation[1].onclick = () => {
+
+    featuresNavigation[1].classList.add('active');
+    featuresNavigation[2].classList.remove('active');
+    featuresNavigation[0].classList.remove('active');
+
     skills.style.display = 'block';
     projects.style.display = 'none';
-    contact.style.display = 'none'
+    contact.style.display = 'none';
 }
 
 featuresNavigation[2].onclick = () => {
+
+    featuresNavigation[2].classList.add('active');
+    featuresNavigation[0].classList.remove('active');
+    featuresNavigation[1].classList.remove('active');
+
     contact.style.display = 'block';
     skills.style.display = 'none';
     projects.style.display = 'none';
 }
+
+
 
